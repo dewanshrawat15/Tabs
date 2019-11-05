d21f6321fd2565ca96ebcada94e1eefa732e9f39
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-11-05 15:51:34 +0530

    Working on home screen, added share mode and dark theme to app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class HomeScreen extends StatefulWidget{
   @override
   HomeScreenState createState() => HomeScreenState();
 }
 
-bool themeState = false;
+bool themeState = true;
 
 class HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: themeState ? ThemeData(brightness: Brightness.light,primaryColor: Colors.deepOrange) : ThemeData(brightness: Brightness.dark, primaryColor: Colors.deepOrange),
+      theme: themeState ? ThemeData(brightness: Brightness.light, primaryColor: Colors.deepOrange) : ThemeData(brightness: Brightness.dark, primaryColor: Colors.deepOrange),
       home: Scaffold(
         appBar: AppBar(
           title: Text(
             "Tabs",
-            style: TextStyle(fontFamily: "Product Sans"),
+            style: TextStyle(fontFamily: "Product Sans", fontSize: 24),
           ),
           actions: <Widget>[
             Padding(
@@ -34,6 +35,9 @@ class HomeScreenState extends State<HomeScreen>{
             Padding(
               padding: EdgeInsets.only(left: 15, right: 20),
               child: InkWell(
+                onTap: (){
+                  Share.share("Hey, I keep tabs on my purchases and transactions using Tabs. Join me by downloading tabs from https://dewanshrawat.tech/");
+                },
                 child: Icon(Icons.share),
               ),
             )
