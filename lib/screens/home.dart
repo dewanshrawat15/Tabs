8c894d2d301f84a39c6f477d270b9dfe80239924
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-11-07 00:04:37 +0530

    Updated app, app now shows credits and debits on home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:share/share.dart';
 import 'package:tabs/components/drawer.dart';
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class HomeScreen extends StatefulWidget{
   @override
   HomeScreenState createState() => HomeScreenState();
@@ -10,6 +13,37 @@ class HomeScreen extends StatefulWidget{
 bool themeState = true;
 
 class HomeScreenState extends State<HomeScreen>{
+
+  connectDatabase() async{
+    Future<Database> database = openDatabase(
+      join(await getDatabasesPath(), 'records.db'),
+      onCreate: (db, version){
+        return db.execute(
+          "CREATE TABLE recordHistory(payer TEXT, payee TEXT, amount INT, time TEXT)",
+        );
+      },
+      version: 1
+    );
+    return database;
+  }
+
+  Future<List<Map<String, dynamic>>> getCredit() async{
+    final Database db = await connectDatabase();
+    var result = db.rawQuery('SELECT SUM(amount) as CREDIT FROM recordHistory where amount > 0');
+    return result;
+  }
+
+  Future<List<Map<String, dynamic>>> getDebit() async{
+    final Database db = await connectDatabase();
+    var result = db.rawQuery('SELECT SUM(amount) as DEBIT FROM recordHistory where amount < 0');
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -45,6 +79,68 @@ class HomeScreenState extends State<HomeScreen>{
           ],
         ),
         drawer: SlideInDrawer(themeState: themeState,),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 32, left: 20),
+                child: FutureBuilder(
+                  future: getCredit(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot){
+                    if(snapshot.hasData){
+                      return ListTile(
+                        leading: CircleAvatar(
+                          radius: 36,
+                          backgroundColor: Colors.deepOrange,
+                          child: Icon(Icons.attach_money, color: Colors.white,),
+                        ),
+                        title: Text("Money you get back"),
+                        subtitle: Text(
+                          (snapshot.data[0]['CREDIT']).toString(),
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontFamily: "Product Sans"
+                          ),
+                        ),
+                      );
+                    }
+                    else{
+                      return Text("Give loans to see them over here.");
+                    }
+                  }
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 32, left: 20),
+                child: FutureBuilder(
+                  future: getDebit(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot){
+                    if(snapshot.hasData){
+                      return ListTile(
+                        leading: CircleAvatar(
+                          radius: 36,
+                          backgroundColor: Colors.deepOrange,
+                          child: Icon(Icons.attach_money, color: Colors.white,),
+                        ),
+                        title: Text("Money you have to pay"),
+                        subtitle: Text(
+                          (-snapshot.data[0]['DEBIT']).toString(),
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontFamily: "Product Sans"
+                          ),
+                        ),
+                      );
+                    }
+                    else{
+                      return Text("Take loans to see them over here.");
+                    }
+                  }
+                )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
